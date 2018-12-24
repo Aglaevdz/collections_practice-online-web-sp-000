090e27f70ccd562b860f4671248c9cda19e4184f
Aglae Verduzco <aglae.cv@gmail.com>
2018-12-24 00:38:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,3 +2,8 @@ def sort_array_asc(array)
   result = array.sort {|left, right| left.length <=> right.length}
   puts result
   end
+
+  def sort_array_desc(array)
+    result = array.sort {|right, left| right.length <=> left.length}
+    puts result
+    end
