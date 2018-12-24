259908d73c961f98784713b743f3fa474bdc7373
Aglae Verduzco <aglae.cv@gmail.com>
2018-12-24 02:27:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,7 +38,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index| }
+  array.each_with_index.collect{|element, index|}
   if index == index[1]
     element
   else
