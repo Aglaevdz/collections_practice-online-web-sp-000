8c96a103aaf41e01b1caff78210403396cd56193
Aglae Verduzco <aglae.cv@gmail.com>
2018-12-24 02:29:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,7 +38,8 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect do |element, index|}  if index == index[1]
+  array.each_with_index.collect do |element, index|  
+    if index == index[1]
     element
   else
     element << "s"
